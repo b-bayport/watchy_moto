f574a3f6677af993e86e438cbe5aa662cbb43652
bayport <121366909+b-bayport@users.noreply.github.com>
2023-06-18 23:04:51 -0400

    Update z900rscafe.h - added time

--- z900rscafe.h
+++ z900rscafe.h
@@ -6,4 +6,23 @@ void WatchyMoto::drawZ900RSCafe(bool light, float batt) {
     display.setTextColor(light ? GxEPD_BLACK : GxEPD_WHITE);
     display.setTextWrap(false);
 
+    // Draw Time /////////////////////////////////////////////////////////////////////////////// Draw Time //
+    display.setFont(&CFWilliamWallace_Regular26pt7b);
+    display.setCursor(25, 199);    
+    
+    int displayHour;
+    if(currentTime.Hour == 0){
+      displayHour = 12;
+    } else {
+      displayHour = currentTime.Hour <=12 ? currentTime.Hour : currentTime.Hour - 12;
+    }
+    display.print(displayHour);
+   
+    display.print(":");
+    
+    if(currentTime.Minute < 10){
+        display.print("0");
+    }
+    display.println(currentTime.Minute);
+
 }
